dacd1c414c6c6772a74424ce3c0cd64199002f19
Asif Patel <asif987patel@gmail.com>
2023-07-02 14:30:04 +0530

    added: some more test

--- main.js
+++ main.js
@@ -169,37 +169,37 @@ function pickRandom(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-// function getCarFrontTexture() {
-//     const canvas = document.createElement("canvas");
-//     canvas.width = 64;
-//     canvas.height = 32;
-//     const context = canvas.getContext("2d");
+function getCarFrontTexture() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 64;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
 
-//     context.fillStyle = "#ffffff";
-//     context.fillRect(0, 0, 64, 32);
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 64, 32);
 
-//     context.fillStyle = "#666666";
-//     context.fillRect(8, 8, 48, 24);
+    context.fillStyle = "#666666";
+    context.fillRect(8, 8, 48, 24);
 
-//     return new THREE.CanvasTexture(canvas);
+    return new THREE.CanvasTexture(canvas);
 
-// }
+}
 
-// function getCarSideTexture() {
-//     const canvas = document.createElement("canvas");
-//     canvas.width = 128;
-//     canvas.height = 32;
-//     const context = canvas.getContext("2d");
+function getCarSideTexture() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 128;
+    canvas.height = 32;
+    const context = canvas.getContext("2d");
 
-//     context.fillStyle = "#ffffff";
-//     context.fillRect(0, 0, 128, 32);
+    context.fillStyle = "#ffffff";
+    context.fillRect(0, 0, 128, 32);
 
-//     context.fillStyle = "#666666";
-//     context.fillRect(10, 8, 38, 24);
-//     context.fillRect(58, 8, 60, 24);
+    context.fillStyle = "#666666";
+    context.fillRect(10, 8, 38, 24);
+    context.fillRect(58, 8, 60, 24);
 
-//     return new THREE.CanvasTexture(canvas);
-// }
+    return new THREE.CanvasTexture(canvas);
+}
 
 function getTruckFrontTexture() {
     const canvas = document.createElement("canvas");
